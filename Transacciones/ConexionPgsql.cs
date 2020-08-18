ba814ada81a662496260634f4f9eadd82977b683
gibranjulyano11 <gibranjulyano11@gmail.com>
2020-08-17 19:11:18 -0500

    Transacciones

---
+++ Transacciones/ConexionPgsql.cs
@@ -0,0 +1,67 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Transacciones
+{
+    class ConexionPgsql
+    {
+        NpgsqlConnection conexion = new NpgsqlConnection("Server = localhost; User Id = postgres; Password = 1110; Database = Db_Transacciones;");
+
+        public DataTable Consultar()
+        {
+            string query = "SELECT  *  FROM clientes ORDER BY id";
+            NpgsqlCommand conector = new NpgsqlCommand(query, conexion);
+            NpgsqlDataAdapter datos = new NpgsqlDataAdapter(conector);
+            DataTable tabla = new DataTable();
+            datos.Fill(tabla);
+            return tabla;
+        }
+
+        public void Transferir(string textBox1, string textBox2, int textBox3)
+        {
+            try
+            {
+                conexion.Open();
+                string query1 = "BEGIN";
+                NpgsqlCommand ejecutor1 = new NpgsqlCommand(query1, conexion);
+                ejecutor1.ExecuteNonQuery();
+
+                string query2 = $"UPDATE clientes set saldos= saldos-{textBox3} WHERE cuenta = '{textBox2}' AND saldos>= {textBox3}";
+                NpgsqlCommand ejecutor2 = new NpgsqlCommand(query2, conexion);
+                var e = ejecutor2.ExecuteNonQuery();
+
+                string query3 = $"UPDATE clientes set saldos= saldos+ {textBox3} WHERE cuenta = '{textBox1}'";
+                NpgsqlCommand ejecutor3 = new NpgsqlCommand(query3, conexion);
+                var f = ejecutor3.ExecuteNonQuery();
+
+                if (f == 1 && e == 1)
+                {
+                    string query4 = "COMMIT";
+                    NpgsqlCommand ejecutor4 = new NpgsqlCommand(query4, conexion);
+                    ejecutor4.ExecuteNonQuery();
+                    conexion.Close();
+                    MessageBox.Show("La transacción se realizó exitosamente.");
+                }
+                else
+                {
+                    string query4 = "ROLLBACK";
+                    NpgsqlCommand ejecutor4 = new NpgsqlCommand(query4, conexion);
+                    ejecutor4.ExecuteNonQuery();
+                    conexion.Close();
+                    MessageBox.Show("La transacción no se realizó.");
+                }
+            }
+            catch (Exception)
+            {
+                conexion.Close();
+                MessageBox.Show("¡Ha ocurrido un problema!, inténtalo nuevamente.");
+            }
+        }
+    }
+}
---
+++ Transacciones/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Transacciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ConexionPgsql conectandoseBD = new ConexionPgsql();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = conectandoseBD.Consultar();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conectandoseBD.Transferir(textBox1.Text, textBox2.Text, Convert.ToInt32(textBox3.Text));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Debe ingresar los datos correctamente.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
